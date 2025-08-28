e39d7c46600c5b382fa0ac6b6c2e97d401b7ab4b
exiler <shaukoay.dev@gmail.com>
2025-08-28 11:30:18 +0800

    Added part 2 and reorganised into folders

---
+++ Part2/api.py
@@ -0,0 +1,12 @@
+# simple app that makes  requests to https://numbersapi.com/
+# fetches trivia about a given number
+import requests
+
+def get_fact(number):
+    url = f"http://numbersapi.com/{number}"
+
+    r = requests.get(url)
+    if r.status_code == 200:
+        print(r.text)
+    else:
+        print(f"An error occurred, code={r.status_code}")
